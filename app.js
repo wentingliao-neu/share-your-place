4c873e93d566ae22bb3ea6b66111ae03f855ca2f
wentingliao-neu <liao.went@northeastern.edu>
2024-01-08 02:05:22 -0500

    adding react frontend

--- app.js
+++ app.js
@@ -10,18 +10,21 @@ const fs = require("fs");
 const app = express();
 app.use(bodyParser.json());
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
-app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
-   res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-   );
-   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-   next();
-});
+app.use(express.static(path.join("public")));
+// app.use((req, res, next) => {
+//    res.setHeader("Access-Control-Allow-Origin", "*");
+//    res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//    );
+//    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//    next();
+// });
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
-
+app.use((req, res, next) => {
+   res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 app.use((req, res, next) => {
    throw new HttpError("could not find this route.", 404);
 });
