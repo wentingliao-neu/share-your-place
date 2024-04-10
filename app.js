5eebb6fe84ec1bcfccae5af7b03286e0263e7c97
wentingliao-neu <liao.went@northeastern.edu>
2024-04-10 01:42:01 -0400

    fix CORS

--- app.js
+++ app.js
@@ -15,17 +15,15 @@ app.use(bodyParser.json());
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 app.use(express.static(path.join("public")));
 
-// app.use((req, res, next) => {
-//    res.setHeader("Access-Control-Allow-Origin", "*");
-//    res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//    );
-//    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-//    next();
-// });
-
-//app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use((req, res, next) => {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+   );
+   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+   next();
+});
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
